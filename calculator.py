f9576c28e4f051cc783880ff2fce457e00312379
Kirk Vinas <k.vinas@Kirks-MacBook-Pro.local>
2025-07-30 21:13:29 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,21 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
-
-
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a,b):
+    a * b
+def div(a,b):
+    try:
+        return b / a
+    except ZeroDivisionError as e:
+        raise
+        print("Caught ZeroDivisionError:", e)
+def log(a,b):
+    try:
+        math.log(a,b)
+    except ValueError as e:
+        print("Caught ValueError:", e)
+def exp(a,b):
+    return a ** b
