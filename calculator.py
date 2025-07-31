4c00f4d8cb8e5f8cb8f1ca3e59803b1dadbaf7b8
GarrettUrso <garrett.urso@ufl.edu>
2025-07-30 21:13:08 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,25 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
 
+def sub(a, b):
+    return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return b / a
+    except ZeroDivisionError as e:
+        print("Caught ZeroDivisionError as:", e)
+
+def log(a, b):
+    try:
+        return math.log(b, a)
+    except ValueError as e:
+        print("Caught ValueError as:", e)
+
+def exp(a, b):
+    return a ** b
